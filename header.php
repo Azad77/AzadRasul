6ffea460fa811845ecc07d153c8c05a55204e847
Dr Azad Rasul <azad.rasul@soran.edu.iq>
2020-08-25 22:55:59 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<!--	
+-->
+<html>
+	<head>
+    <div id='cssmenu'>
+			<ul>
+				<li>
+					<a href="https://azad77.github.io/AzadRasul/" target="_blank">Home</a>
+				</li>
+				<li>
+					<a href="https://azad77.github.io/AzadRasul/About.html" target="_blank">About</a>
+				</li>
+				<li>
+					<a href="https://azad77.github.io/AzadRasul/Resume.html" target="_blank">Resume</a>
+				</li>
+				<li>
+					<a href="https://azad77.github.io/AzadRasul/Publications.html" target="_blank">Publications</a>
+				</li>
+				<li>
+					<a class="active" href="https://azad77.github.io/AzadRasul/Contact.html" target = "_blank">Contact</a>
+				</li>
+			</ul>
+		</div>
+    </head>
+    </html>
