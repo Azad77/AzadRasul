ee0063db49e3ddbc61dadcd2977b9935cfb68130
Dr Azad Rasul <azad.rasul@soran.edu.iq>
2020-08-26 00:40:32 +0300

    Update and rename Contact.md to ontact.php

---
+++ ontact.php
@@ -0,0 +1,16 @@
+    <?php
+        include('includes/header.php');
+    ?>
+    <main>
+        <h2>Contact Page with the same header as the homepage</h2>
+        <ul>
+            <li>Phone: 123 456 7890</li>
+            <li>Fax: 555 555 1234</li>
+            <li><a href="email@email.com">email@email.com</a></li>
+            <li>Homing Pigeon: You'll need one of our pigeons…</li>
+            <li>Owl: Muggles need not inquire</li>
+        </ul>
+    </main>
+</body>
+</html>
+
